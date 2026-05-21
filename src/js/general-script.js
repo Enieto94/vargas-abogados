c32e3adf9fe90cb76a53bb23e71133bc6c8aa4a9
=Edwin Nieto <=egnieto94@gmail.com>
2026-05-21 17:09:11 -0500

    Responsive implemented

--- src/js/general-script.js
+++ src/js/general-script.js
@@ -2,4 +2,32 @@ $(document).ready(function () {
 	$('#btn-conoce').click(function () {
 		$('#nuestro-equipo .cards').toggleClass('active');
 	});
+
+	$('.servicios-carousel').owlCarousel({
+		loop: false,
+		margin: 20,
+		dots: true,
+		nav: false,
+		autoplay: false,
+		autoplayHoverPause: true,
+		responsive: {
+			0: { items: 1 },
+			576: { items: 1 },
+			768: { items: 2 },
+			992: { items: 3 },
+			1200: { items: 4 }
+		}
+	});
+
+	$('.burger-button').click(function () {
+		$('.nav-wrapper').toggleClass('nav-open');
+		$(this).toggleClass('open');
+	});
+
+	$('.nav-links a').click(function () {
+		if ($('.nav-wrapper').hasClass('nav-open')) {
+			$('.nav-wrapper').removeClass('nav-open');
+			$('.burger-button').removeClass('open');
+		}
+	});
 });
